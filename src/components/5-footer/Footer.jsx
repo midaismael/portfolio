ecd2c0082049ab30b6b775bb50ff0cc06a15dddc
midaismael <midaismael@gmail.com>
2024-07-01 23:22:40 +0300

    footer

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import './footer.css';
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      footer      
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a  href="">About</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Speaking</a>
+        </li>
+        <li>
+          <a href="">Uses</a>
+        </li>
+      </ul>
+      <p>© 2024 spencer sharp. All right reserved. </p>
     </footer>
-  )
-} 
+  );
+};
 export default Footer;
