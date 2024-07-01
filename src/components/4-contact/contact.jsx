384af4ae99cd7731033641871b77ccde349c3018
midaismael <midaismael@gmail.com>
2024-07-01 23:06:09 +0300

    contact-us

--- src/components/4-contact/contact.jsx
+++ src/components/4-contact/contact.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import './contact.css'
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      contact
-    </div>
-  )
-}
+    <section className="contact-us">
+      <h1 className="title">
+        <span className="icon-envelope"></span>
+        Contact Us
+      </h1>
+      <p className="sub-title">
+        Contact us for more information and get notified when i puplish
+        something new.
+      </p>
+      <div className="flex">
+        <form className="">
+          <div className="flex">
+            <label htmlFor="email">Email Address:</label>
+            <input required type="email" name="" id="email" />
+          </div>
+          <div className="flex" style={{marginTop:"24px"}}>
+            <label htmlFor="message">Your message:</label>
+            <textarea required name="" id="message"></textarea>
+          </div>
+          <button className="submit">Submit</button>
+        </form>
+        <div className="animation border">animation</div>
+      </div>
+    </section>
+  );
+};
 export default Contact;
