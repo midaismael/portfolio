b25a02cae813c72ea8f8446e7786f0156d34cf6a
midaismael <midaismael@gmail.com>
2024-07-02 22:56:56 +0300

    hover

--- src/components/4-contact/contact.jsx
+++ src/components/4-contact/contact.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./contact.css";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("manwyopr");
   return (
     <section className="contact-us">
       <h1 className="title">
@@ -13,16 +15,33 @@ const Contact = () => {
         something new.
       </p>
       <div className="flex">
-        <form className="">
+        <form onSubmit={handleSubmit} className="">
           <div className="flex">
             <label htmlFor="email">Email Address:</label>
-            <input required type="email" name="" id="email" />
+            <input autoComplete="off" required type="email" name="email" id="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
-          <div className="flex" style={{marginTop:"24px"}}>
+          <div className="flex" style={{ marginTop: "24px" }}>
             <label htmlFor="message">Your message:</label>
-            <textarea required name="" id="message"></textarea>
+            <textarea required name="message" id="message"></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
-          <button className="submit">Submit</button>
+          <button className="submit" type="submit" disabled={state.submitting}>
+            {state.submitting ? "Submitting ..." : "Submit"}
+          </button>
+          {state.succeeded && (
+            <p style={{ fontSize: "16px", marginTop: "1.7rem" }}>
+              Your message has been sent successfully 👌
+            </p>
+          )}
         </form>
         <div className="animation border">animation</div>
       </div>
